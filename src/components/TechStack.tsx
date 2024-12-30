2d344eb6523ed2443c43f3bc4fbd7d10b13c7dbb
abdulrg01 <abdulrg01@gmail.com>
2024-12-30 09:27:00 +0100

    images

--- src/components/TechStack.tsx
+++ src/components/TechStack.tsx
@@ -7,43 +7,43 @@ export const TechStack = () => {
   const stack = [
     {
       title: "Next.js",
-      src: "/images/logos/next.png",
+      src: "../public/logos/next.png",
 
       className: "h-10 w-14",
     },
     {
       title: "AWS",
-      src: "/images/logos/aws.webp",
+      src: "../public/logos/aws.webp",
 
       className: "h-10 w-10",
     },
     {
       title: "Figma",
-      src: "/images/logos/figma.png",
+      src: "../public/logos/figma.png",
 
       className: "h-10 w-8",
     },
     {
       title: "Framer Motion",
-      src: "/images/logos/framer.webp",
+      src: "../public/logos/framer.webp",
 
       className: "h-10 w-10",
     },
     {
       title: "Node",
-      src: "/images/logos/node.png",
+      src: "../public/logos/node.png",
 
       className: "h-10 w-12",
     },
     {
       title: "Tailwind",
-      src: "/images/logos/tailwind.png",
+      src: "../public/logos/tailwind.png",
 
       className: "h-10 w-24",
     },
     {
       title: "Vercel",
-      src: "/images/logos/vercel.png",
+      src: "../public/logos/vercel.png",
 
       className: "h-10 w-24",
     },
