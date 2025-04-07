869bec12a39364e32a3295abaa9c4d619667f66b
abdulrg01 <abdulrg01@gmail.com>
2025-04-07 12:37:56 +0100

    new projects

--- src/constants/products.tsx
+++ src/constants/products.tsx
@@ -30,8 +30,75 @@ import airbnb from "../public/airbnb.png";
 import airbnb2 from "../public/airbnb2.png";
 import photoai from "../public/photoai.png";
 import photoai2 from "../public/photoai2.png";
+import nak1 from "../public/nak1.png";
+import nak2 from "../public/nak2.png";
+import nak3 from "../public/nak3.png";
+import kainuwa1 from "../public/kainuwa1.png";
+import kainuwa2 from "../public/kainuwa2.png";
+import kainuwa3 from "../public/kainuwa3.png";
 
 export const products = [
+  {
+    href: "https://webpages-one.vercel.app/kainuwa",
+    title: "Kainuwa",
+    description:
+      "Global pharmacy",
+    thumbnail: kainuwa1,
+    images: [kainuwa2, kainuwa3],
+    stack: ["React", "Tailwindcss", "Aceternity"],
+    slug: "kainuwa",
+    iconLists: [reactImg, TailwindImg],
+    content: (
+      <div>
+        <p>
+          Sit eiusmod ex mollit sit quis ad deserunt. Sint aliqua aliqua ullamco
+          dolore nulla amet tempor sunt est ipsum. Dolor laborum eiusmod
+          cupidatat consectetur velit ipsum. Deserunt nisi in culpa laboris
+          cupidatat elit velit aute mollit nisi. Officia ad exercitation laboris
+          non cupidatat duis esse velit ut culpa et.{" "}
+        </p>
+        <p>
+          Exercitation pariatur enim occaecat adipisicing nostrud adipisicing
+          Lorem tempor ullamco exercitation quis et dolor sint. Adipisicing sunt
+          sit aute fugiat incididunt nostrud consequat proident fugiat id.
+          Officia aliquip laborum labore eu culpa dolor reprehenderit eu ex enim
+          reprehenderit. Cillum Lorem veniam eu magna exercitation.
+          Reprehenderit adipisicing minim et officia enim et veniam Lorem
+          excepteur velit adipisicing et Lorem magna.
+        </p>{" "}
+      </div>
+    ),
+  },
+  {
+    href: "https://nak-theta.vercel.app/nakacademy",
+    title: "Nak Academy",
+    description: "Welcome to Nak Academy. An independent day and Islamic school for ages 0 – 18. Your Journey",
+    thumbnail: nak1,
+    images: [nak2, nak3],
+    stack: ["React", "Tailwindcss", "Aceternity"],
+    slug: "nakacademy",
+    iconLists: [reactImg, TailwindImg],
+    content: (
+      <div>
+        <p>
+          Sit eiusmod ex mollit sit quis ad deserunt. Sint aliqua aliqua ullamco
+          dolore nulla amet tempor sunt est ipsum. Dolor laborum eiusmod
+          cupidatat consectetur velit ipsum. Deserunt nisi in culpa laboris
+          cupidatat elit velit aute mollit nisi. Officia ad exercitation laboris
+          non cupidatat duis esse velit ut culpa et.{" "}
+        </p>
+        <p>
+          Exercitation pariatur enim occaecat adipisicing nostrud adipisicing
+          Lorem tempor ullamco exercitation quis et dolor sint. Adipisicing sunt
+          sit aute fugiat incididunt nostrud consequat proident fugiat id.
+          Officia aliquip laborum labore eu culpa dolor reprehenderit eu ex enim
+          reprehenderit. Cillum Lorem veniam eu magna exercitation.
+          Reprehenderit adipisicing minim et officia enim et veniam Lorem
+          excepteur velit adipisicing et Lorem magna.
+        </p>{" "}
+      </div>
+    ),
+  },
   {
     href: "https://webpages-one.vercel.app/",
     title: "PhotoAI",
